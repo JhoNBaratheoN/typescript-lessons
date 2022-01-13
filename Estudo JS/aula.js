a2f1366591e3e93ab803c4a066234a38a1fc3105
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2022-01-13 17:38:19 -0300

    Add files via upload
    Só minha primeira aula de TypeScript.

---
+++ Estudo JS/aula.js
@@ -0,0 +1,9 @@
+//let idade = 5;
+//console.log(idade);
+//let altura = 180;
+//console.log(altura);
+//let nome = "João";
+
+let valorIngressoAdulto = 20;
+valorIngressoAdulto = 30;
+console.log(valorIngressoAdulto);
